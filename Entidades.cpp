5d72369b80ce9127e3636ecebba7abda90f95283
RedmanFernando016 <az562695@gmail.com>
2026-02-18 16:32:18 -0300

    estruturação de movimento e ações de entidades

--- Entidades.cpp
+++ Entidades.cpp
@@ -2,3 +2,36 @@
 #include <iostream>
 using namespace std; 
 
+void MovimentoJogador(std::vector<Mapa> &mapas){
+    bool ValidacaoMovimento = false; 
+    char movimento;
+    int idx;
+
+
+    do{    
+        cout << "Digite o movimento: \n";
+        cin >> movimento;
+
+        for(int i = 0; i < mapas.size(); i++){
+            if(mapas[i].bloco == '@');
+            idx = i;
+
+            if(movimento == 'w' || movimento =='W'){
+                if(mapas[idx - 20].bloco == '|' || idx - 20 < mapas.size()){
+                    ValidacaoMovimento = false;
+                    cout << "\nMovimento inválido, tente novamente! \n";
+                } else {
+                    mapas[idx].bloco = '.';
+                    mapas[idx - 20].bloco = '@';
+                    return;
+                }
+            }
+
+            if(movimento == 'a' || movimento == 'A'){
+                
+            }
+        }
+
+    } while (ValidacaoMovimento == false);
+}
+
--- head.h
+++ head.h
@@ -1,18 +1,47 @@
 #ifndef HEAD_H
 #define HEAD_H
 #include <vector> 
+using namespace std;
+
+// Funções do mapa
 
 struct Mapa{
     char bloco; 
 };
 
-// Funções do mapa
 void Criador(std::vector<Mapa> &mapas); 
 void Imprimir(std::vector<Mapa> &mapas);
 void CaminhoGarantido(std::vector<Mapa> &mapas);
 void DecidirCaminho(std::vector<Mapa> &mapas, int largura, int &idx, int destino);
 
+
+
 //Funções das entidades
+
+struct Item{
+    string nome;
+    int quantidade;
+};
+
+struct Jogador{
+    int vida, estamina;
+    string ataques;
+
+    vector<Item> inventario;
+};
+
+struct Monstro{
+    int vida, estamina; 
+    string ataques; 
+    
+};
+
+void MovimentoJogador(std::vector<Mapa> &mapas);
+void MovimentoMonstro(std::vector<Mapa> &mapas);
+void PegarItem(std::vector<Item> &inventario, const Item &item);
+void AbrirInventario(std::vector<Item> &inventario, const Item &item);
+void LargarItem(std::vector<Item> &inventario, const Item &item);
+
 
 
 #endif
